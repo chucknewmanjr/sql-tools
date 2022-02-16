fb615391fffd78719bef09d625d3c01c102c399d
Chuck Newman, Jr <33396894+chucknewmanjr@users.noreply.github.com>
2022-02-16 17:43:51 -0500

    Update Generate-Column-SQL-for-Row-Generators.sql

--- SQL-Code-Generators/Generate-Column-SQL-for-Row-Generators.sql
+++ SQL-Code-Generators/Generate-Column-SQL-for-Row-Generators.sql
@@ -54,23 +54,22 @@ with
 select
 	col.column_id,
 	col.column_name,
-	col.typ,
 	REPLACE(template.template, '[name]', 't.[' + col.column_name + ']') + ''', '',' + IIF(col.is_identity = 1, ' -- identity', '')
 from col
 left join type_group on col.typ = type_group.typ
 join (values
-	('num', 0, NULL, '[name], '), -- TESTED
+	('num', 0, NULL, '[name], '),
 	('bin', 0, NULL, 'CONVERT(varchar(MAX), [name], 1), '),
-	('quo', 0, NULL, @4q + ', [name], ' + @4q + ', '), -- TESTED
+	('quo', 0, NULL, @4q + ', [name], ' + @4q + ', '),
 	('oth', 0, 1,    @4q + ', [name], ' + @4q + ', '), -- A quote is unlikely in 18 or fewer characters.
-	('oth', 0, 2,    @4q + ', QUOTENAME([name]), ' + @4q + ', '), -- QUOTENAME is nicer
-	('oth', 0, 3,    @4q + ', REPLACE([name], ' + @4q + ', ' + @6q + '), ' + @4q + ', '), -- TESTED
-	('num', 1, NULL, 'ISNULL(CAST([name] AS varchar(MAX)), ''NULL''), '), -- TESTED
-	('bin', 1, NULL, 'ISNULL(CONVERT(varchar(MAX), [name], 1), ''NULL''), '), -- TESTED
-	('quo', 1, NULL, 'ISNULL(' + @4q + ' + CAST([name] AS varchar(MAX)) + ' + @4q + ', ''NULL''), '), -- TESTED
-	('oth', 1, 1,    'ISNULL(' + @4q + ' + [name] + ' + @4q + ', ''NULL''), '),
-	('oth', 1, 2,    'ISNULL(' + @4q + ' + QUOTENAME([name]) + ' + @4q + ', ''NULL''), '),
-	('oth', 1, 3,    'ISNULL(' + @4q + ' + REPLACE([name], ' + @4q + ', ' + @6q + ') + ' + @4q + ', ''NULL''), ') -- TESTED
+	('oth', 0, 2,    @4q + ', QUOTENAME([name]), ' + @4q + ', '), -- QUOTENAME is nicer.
+	('oth', 0, 3,    @4q + ', REPLACE([name], ' + @4q + ', ' + @6q + '), ' + @4q + ', '),
+	('num', 1, NULL, 'ISNULL(CAST([name] AS varchar(MAX)), ''NULL''), '),
+	('bin', 1, NULL, 'ISNULL(CONVERT(varchar(MAX), [name], 1), ''NULL''), '),
+	('quo', 1, NULL, 'ISNULL(' + @4q + ' + CAST([name] AS varchar(MAX)) + ' + @4q + ', ''NULL''), '),
+	('oth', 1, 1,    'ISNULL(' + @4q + ' + [name] + ' + @4q + ', ''NULL''), '), -- A quote is unlikely in 18 or fewer characters.
+	('oth', 1, 2,    'ISNULL(' + @4q + ' + QUOTENAME([name]) + ' + @4q + ', ''NULL''), '), -- QUOTENAME is nicer.
+	('oth', 1, 3,    'ISNULL(' + @4q + ' + REPLACE([name], ' + @4q + ', ' + @6q + ') + ' + @4q + ', ''NULL''), ')
 ) template (grp, is_nullable, length_type, template)
 	on isnull(type_group.grp, 'oth') = template.grp 
 	and col.is_nullable = template.is_nullable 
