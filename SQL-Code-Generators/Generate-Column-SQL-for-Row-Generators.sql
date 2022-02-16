1a94a88c716ebe48d4f4b09f12f413b26b2d5b87
Chuck Newman, Jr <33396894+chucknewmanjr@users.noreply.github.com>
2022-02-16 17:42:10 -0500

    Update Generate-Column-SQL-for-Row-Generators.sql

--- SQL-Code-Generators/Generate-Column-SQL-for-Row-Generators.sql
+++ SQL-Code-Generators/Generate-Column-SQL-for-Row-Generators.sql
@@ -1,57 +1,94 @@
-drop table if exists #c -- drop old table
-go
-
-select top 0 * into #c from sys.columns -- make new table
-go
-
 -- ==================================
 -- ===== Put_Your_Query_In_Here =====
 
 declare @Put_Your_Query_In_Here nvarchar(MAX) = '
-	SELECT
-		cl.ClientId, 
-		cl.[Name], 
-		cl.ClientComment, 
-		cl.SyncId, 
-		r.RegionCode
-	FROM [Core].[Client] cl
-	join [Core].[Region] r on cl.HostRegionId = r.RegionId
-	where cl.IsActive = 1
-'
+	select is_published, is_encrypted, create_date, database_id,
+		resource_pool_id, default_language_name, [name],
+		collation_name, service_broker_guid, replica_id, owner_sid
+	from sys.databases
+';
+
+-- ==================================
+-- ========== INSTRUCTIONS ==========
+
+-- The output is SQL code that goes inside of CONCAT().
+
 -- ==================================
 -- ==================================
 
-insert #c exec ('select top 0 * into #q from (' + @Put_Your_Query_In_Here + ') t; select * from tempdb.sys.columns where object_id = object_id(''tempdb..#q'')')
+drop table if exists #column; -- drop table
+
+select top 0 * into #column from sys.columns; -- recreate the table
+
+-- insert rows about columns
+insert #column exec ('select top 0 * into #q from (' + @Put_Your_Query_In_Here + ') t; select * from tempdb.sys.columns where object_id = object_id(''tempdb..#q'')');
 go
 
-select 
-	c.column_id,
-	c.[name],
-	REPLACE(t.template, '[name]', 't.' + c.[name]) + ''', '', ' + IIF(c.is_identity = 1, '-- identity', '')
-from #c c 
-join (values 
-	(1, 'int smallint bit bigint tinyint decimal numeric'), -- no quotes
-	(2, 'datetime date datetime2'), -- quotes but no replace
-	(3, 'varchar nvarchar char uniqueidentifier xml'), -- might replace quotes
-	(4, 'varbinary')
-) dtg (data_type_group_id, data_type_list) 
-	on dtg.data_type_list like '%' + TYPE_NAME(c.user_type_id) + '%'
+declare @4q char(4) = REPLICATE('''', 4); -- 4 quotes in a row becomes a single quote
+declare @6q char(6) = REPLICATE('''', 6); -- 6 quotes in a row becomes 2 single quotes
+
+with
+	col as (
+		select
+			column_id,
+			[name] as column_name,
+			TYPE_NAME(user_type_id) as typ,
+			is_nullable,
+			is_identity,
+			case
+				when max_length between 1 and 18 then 1
+				when max_length between 19 and 128 then 2
+				else 3
+			end as length_type
+		from #column
+	),
+	type_group as (
+		-- numbers get no quotes or replacements
+		select 'num' as grp, * from (values ('bit'), ('tinyint'), ('smallint'), ('int'), ('bigint'), ('decimal'), ('numeric'), ('real'), ('float'), ('money'), ('smallmoney')) t (typ)
+		-- binaries get converted and no quotes
+		union select 'bin', * from (values ('binary'), ('varbinary'), ('timestamp')) t (typ)
+		-- dates and such get quotes but no replacements
+		union select 'quo', * from (values ('datetime'), ('date'), ('datetime2'), ('time'), ('datetimeoffset'), ('smalldatetime'), ('uniqueidentifier'), ('hierarchyid')) t (typ)
+		-- varchar and any remaining types get single quotes replaced with REPLACE or QUOTENAME.
+	)
+select
+	col.column_id,
+	col.column_name,
+	col.typ,
+	REPLACE(template.template, '[name]', 't.[' + col.column_name + ']') + ''', '',' + IIF(col.is_identity = 1, ' -- identity', '')
+from col
+left join type_group on col.typ = type_group.typ
 join (values
-	(1, 0, 0, '[name], '),
-	(2, 0, 0, ''''''''', [name], '''''),
-	(3, 0, 0, ''''''''', [name], '''''),
-	(3, 1, 0, ''''''''', REPLACE([name], '''''''', ''''''''''''), '''''),
-	(4, 0, 0, ''''''''', CONVERT(varchar(MAX), [name], 1), '''''''', '),
-	(1, 0, 1, 'ISNULL(CAST([name] AS varchar(MAX)), ''NULL''), '),
-	(2, 0, 1, 'ISNULL('''''''' + CAST([name] AS varchar(MAX)) + '''''''', ''NULL''), '),
-	(3, 0, 1, 'ISNULL('''''''' + CAST([name] AS nvarchar(MAX)) + '''''''', ''NULL''), '),
-	(3, 1, 1, 'ISNULL('''''''' + REPLACE([name], '''''''', '''''''''''') + '''''''', ''NULL''), '),
-	(4, 0, 1, 'ISNULL('''''''' + CONVERT(varchar(MAX), [name], 1) + '''''''', ''NULL''), ')
-	-- To-Do: Use QUOTENAME(value, '''') instead of REPLACE for columns under 128 characters wide.
-) t (data_type_group_id, is_long, is_nullable, template) 
-	on dtg.data_type_group_id = t.data_type_group_id
-	and IIF(c.max_length between 1 and 18, 0, 1) = 0 -- uniqueidentifier max length is 16
-	and c.is_nullable = t.is_nullable
-order by c.column_id
-go
+	('num', 0, NULL, '[name], '), -- TESTED
+	('bin', 0, NULL, 'CONVERT(varchar(MAX), [name], 1), '),
+	('quo', 0, NULL, @4q + ', [name], ' + @4q + ', '), -- TESTED
+	('oth', 0, 1,    @4q + ', [name], ' + @4q + ', '), -- A quote is unlikely in 18 or fewer characters.
+	('oth', 0, 2,    @4q + ', QUOTENAME([name]), ' + @4q + ', '), -- QUOTENAME is nicer
+	('oth', 0, 3,    @4q + ', REPLACE([name], ' + @4q + ', ' + @6q + '), ' + @4q + ', '), -- TESTED
+	('num', 1, NULL, 'ISNULL(CAST([name] AS varchar(MAX)), ''NULL''), '), -- TESTED
+	('bin', 1, NULL, 'ISNULL(CONVERT(varchar(MAX), [name], 1), ''NULL''), '), -- TESTED
+	('quo', 1, NULL, 'ISNULL(' + @4q + ' + CAST([name] AS varchar(MAX)) + ' + @4q + ', ''NULL''), '), -- TESTED
+	('oth', 1, 1,    'ISNULL(' + @4q + ' + [name] + ' + @4q + ', ''NULL''), '),
+	('oth', 1, 2,    'ISNULL(' + @4q + ' + QUOTENAME([name]) + ' + @4q + ', ''NULL''), '),
+	('oth', 1, 3,    'ISNULL(' + @4q + ' + REPLACE([name], ' + @4q + ', ' + @6q + ') + ' + @4q + ', ''NULL''), ') -- TESTED
+) template (grp, is_nullable, length_type, template)
+	on isnull(type_group.grp, 'oth') = template.grp 
+	and col.is_nullable = template.is_nullable 
+	and col.length_type = ISNULL(template.length_type, col.length_type)
+order by col.column_id;
 
+-- ==================================
+-- =====      TEST QUERIES      =====
+
+	--select reads, statement_context_id, query_hash, [ansi_nulls],
+	--	start_time, wait_resource, [language], percent_complete,
+	--	connection_id, [context_info]
+	--from sys.dm_exec_requests;
+
+	--select is_published, is_encrypted, create_date, database_id,
+	--	resource_pool_id, default_language_name, [name],
+	--	collation_name, service_broker_guid, replica_id, owner_sid
+	--from sys.databases;
+
+-- ==================================
+-- ==================================
