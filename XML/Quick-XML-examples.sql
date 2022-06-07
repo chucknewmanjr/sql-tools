f2092f7bf8c2ee5bd4e28541bee5f9d6232a1e63
Chuck Newman, Jr <33396894+chucknewmanjr@users.noreply.github.com>
2022-06-07 19:18:32 -0400

    Create Quick-XML-examples.sql

---
+++ XML/Quick-XML-examples.sql
@@ -0,0 +1,15 @@
+-- parse a comma separated list
+DECLARE @list VARCHAR(MAX) = 'Olivia, Emma, Charlotte'
+
+DECLARE @xml XML = '<x>' + REPLACE(@list, ',', '</x><x>') + '</x>'
+
+SELECT LTRIM(c.value('.', 'varchar(max)')) FROM @xml.nodes('x') t (c)
+
+-- parse a comma separated list
+DECLARE @list VARCHAR(MAX) = 'Olivia, Emma, Charlotte'
+
+SELECT LTRIM(t2.c.value('.', 'varchar(max)')) 
+FROM (VALUES (cast('<x>' + REPLACE(@list, ',', '</x><x>') + '</x>' AS XML).query('.'))) t1 (c)
+CROSS APPLY t1.c.nodes('x') t2 (c)
+
+
