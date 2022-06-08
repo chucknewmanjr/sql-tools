79535e6074ec38e0701e53386363422b4df5bdd2
Chuck Newman, Jr <33396894+chucknewmanjr@users.noreply.github.com>
2022-06-08 13:33:44 -0400

    Update Quick-XML-examples.sql

--- XML/Quick-XML-examples.sql
+++ XML/Quick-XML-examples.sql
@@ -1,4 +1,25 @@
 -- ============================================================================
+-- the basics
+DECLARE @Vector XML = '<vectors><vector><x>2</x><y>3</y></vector><vector><x>5</x><y>7</y></vector></vectors>'
+
+SELECT @Vector.query('.')
+
+SELECT c.query('.') FROM @Vector.nodes('.') t (c) -- same
+
+SELECT c.query('.') FROM @Vector.nodes('*') t (c) -- same
+
+SELECT c.query('.') FROM @Vector.nodes('*/*') t (c) -- star means all at the next level
+
+SELECT c.query('..') FROM @Vector.nodes('*/*') t (c) -- dot dot means go back a level
+
+SELECT c.query('*') FROM @Vector.nodes('*/*') t (c) 
+
+SELECT c.query('*') FROM @Vector.nodes('vectors/vector') t (c) 
+
+SELECT c.value('x[1]', 'int'), c.value('y[1]', 'int') FROM @Vector.nodes('vectors/vector') t (c) -- requires a singleton
+
+
+-- ============================================================================
 -- parse a comma separated list
 DECLARE @list VARCHAR(MAX) = 'Olivia, Emma, Charlotte'
 
