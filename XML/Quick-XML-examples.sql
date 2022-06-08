377fdd4bc9c2f73469f17b0038b3069ece17a6f8
Chuck Newman, Jr <33396894+chucknewmanjr@users.noreply.github.com>
2022-06-08 07:52:23 -0400

    Update Quick-XML-examples.sql

--- XML/Quick-XML-examples.sql
+++ XML/Quick-XML-examples.sql
@@ -14,4 +14,11 @@ SELECT LTRIM(t2.c.value('.', 'varchar(max)'))
 FROM (VALUES (CAST('<x>' + REPLACE(@list, ',', '</x><x>') + '</x>' AS XML))) t1 (c)
 CROSS APPLY t1.c.nodes('x') t2 (c)
 
+-- ============================================================================
+-- get tag name
+declare @xml xml = '<x><a>1</a><b>2</b><c>3</c></x>'
+
+select c.query('.'), c.value('.', 'int'), c.value('fn:local-name(.)', 'char(1)')
+from @xml.nodes('x/*') t (c)
+
 
